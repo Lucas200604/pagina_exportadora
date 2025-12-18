86fcabc1ca713b090811d2f2d7bda9b45b4b0332
Lucas Ramirez <ramirezrodriguezlucas@gmail.com>
2025-12-17 23:33:29 -0300

    Actualizacion de js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ const visualHeader = document.getElementById('sticky-header-visual');
     function checkScrollVisual() {
         if (window.scrollY > scrollPoint) {
             // Fondo sólido y sombra al hacer scroll
-            visualHeader.classList.add('bg-lime-950', 'opacity-98', 'shadow-lg');
+            visualHeader.classList.add('bg-emerald-600', 'opacity-98', 'shadow-lg');
             visualHeader.classList.remove('bg-transparent');
         } else {
             // Fondo transparente en la parte superior
-            visualHeader.classList.remove('bg-lime-950', 'opacity-90', 'shadow-lg');
+            visualHeader.classList.remove('bg-emerald-600', 'opacity-90', 'shadow-lg');
             // Nota: Aquí se asume que al inicio no tiene clases de fondo, o podrías usar:
             // visualHeader.classList.add('bg-transparent'); 
         }
